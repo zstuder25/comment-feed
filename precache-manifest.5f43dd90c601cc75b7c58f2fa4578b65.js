1b1765e898e646d61a89f19ad5a6e3e92cf08fb8
Zachery Studer <zachery.j.studer@accenture.com>
2020-06-24 19:01:44 -0400

    Updates

---
+++ precache-manifest.5f43dd90c601cc75b7c58f2fa4578b65.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7a914ae636d4235bb06f3faf2bb057",
+    "url": "/comment-feed/index.html"
+  },
+  {
+    "revision": "8180480a6fca48a303ff",
+    "url": "/comment-feed/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "415cb92e1360502178b8",
+    "url": "/comment-feed/static/css/main.c8b807c9.chunk.css"
+  },
+  {
+    "revision": "8180480a6fca48a303ff",
+    "url": "/comment-feed/static/js/2.c0d8323a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/comment-feed/static/js/2.c0d8323a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "415cb92e1360502178b8",
+    "url": "/comment-feed/static/js/main.152d7de1.chunk.js"
+  },
+  {
+    "revision": "31742cfba682b8a17fd2",
+    "url": "/comment-feed/static/js/runtime-main.22a412fe.js"
+  },
+  {
+    "revision": "7876e7b04959e437de78b39b88fed61c",
+    "url": "/comment-feed/static/media/comment.7876e7b0.svg"
+  },
+  {
+    "revision": "a340eabfa6140d6ad6a8acf5711f7354",
+    "url": "/comment-feed/static/media/delete.a340eabf.svg"
+  },
+  {
+    "revision": "4a5a13c529f840b956401f5154eefc35",
+    "url": "/comment-feed/static/media/edit.4a5a13c5.svg"
+  },
+  {
+    "revision": "ba0cc007965a9b148953ac1a9a695d53",
+    "url": "/comment-feed/static/media/ellipsis.ba0cc007.svg"
+  },
+  {
+    "revision": "6f63e09316ec1a6ec44660e85ad87e55",
+    "url": "/comment-feed/static/media/heart.6f63e093.svg"
+  },
+  {
+    "revision": "8b9e48922316ab5ca9b54e5d0111e2cc",
+    "url": "/comment-feed/static/media/heartGray.8b9e4892.svg"
+  },
+  {
+    "revision": "ccbdd81487c3b3c0e4137ea4f171ec0e",
+    "url": "/comment-feed/static/media/hoverDot.ccbdd814.svg"
+  },
+  {
+    "revision": "2590c4f70fb628e93ebdfef30abe49b5",
+    "url": "/comment-feed/static/media/location.2590c4f7.svg"
+  },
+  {
+    "revision": "00b475eff8028dfc05e3f61f38b89ced",
+    "url": "/comment-feed/static/media/profilePicture.00b475ef.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/comment-feed/precache-manifest.5f43dd90c601cc75b7c58f2fa4578b65.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/comment-feed/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
